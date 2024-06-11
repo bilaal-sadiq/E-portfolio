e219092f44a5bd6a941adeaff601a7be0b4af041
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-06-11 16:52:54 +0100

    Error message for when email is unavaliable

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// template_s3chdi9
-// service_6mszv7l
-// xeNy6f_m8FyMq1rcu
-
 function contact(event) {
-    event.preventDefualt();
-    // emailjs
-    //     .sendform(
-    //         'service_6mszv7l',
-    //         'template_s3chdi9',
-    //         event.target,
-    //         'xeNy6f_m8FyMq1rcu'
-    //     ).then(() => {
-    //         console.log('this worked')
-    //     })
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
     loading.classList += " modal__overlay--visible";
-    setTimeout(() => {
-        loading.classList.remove("modal__overlay--visible")
+    emailjs
+      .sendForm(
+        "service_6mszv7l",
+        "template_s3chdi9",
+        event.target,
+        "PA2g8V0bXo2yvmNuC"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
         success.classList += " modal__overlay--visible";
-        console.log('it worked 1')
-    }, 1000);
-}
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+  }
+
+
+
+
+
